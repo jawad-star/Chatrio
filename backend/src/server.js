162b08e9b67e1833ab4bf3b85dc55d383a131892
jawad-star <jawwadsaleem913@gmail.com>
2025-07-20 22:32:34 +0500

    Refactor server initialization to use async startServer function

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,23 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
-connectDB()
 
-app.get("/",(req,res)=> res.send("hello from the server"))
 
-app.listen(ENV.PORT, () => console.log("server is running on PORT", ENV.PORT));
+app.get("/", (req, res) => res.send("hello from the server"));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+startServer();
