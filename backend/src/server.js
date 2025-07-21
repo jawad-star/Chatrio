a890686f7d0ebcaf68b4b789c9905fb0c77f9f39
jawad-star <jawwadsaleem913@gmail.com>
2025-07-21 06:08:12 +0500

    add packages to handler the async multer and add user.route.js  and add middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("hello from the server"));
 
 const startServer = async () => {
